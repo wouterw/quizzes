2aa7a598216b6d378fac6dbdd87e2807704b0b11
Wouter Willaert <wouterw@Wouters-MacBook-Pro.local>
2013-10-11 18:03:24 +0200

    Initial Commit

---
+++ Quizzes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Quizzes
+//
+//  Created by Wouter Willaert on 11/10/13.
+//  Copyright (c) 2013 Typework. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
